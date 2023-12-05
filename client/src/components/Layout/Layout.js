817251fdd2a95e80d6f3c89e1a31c4adedc85e53
carora2209 <carora2209@gmail.com>
2023-12-05 23:18:37 +0530

    Update Layout.js

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -7,21 +7,21 @@ import { Toaster } from "react-hot-toast";
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <meta name="description" content={description}/>
-      <meta name="keywords" content={keywords} />
-      <meta name="author" content={author} />
-      <title>{ title }</title>
-    </Helmet>
-    <Header />
-    <main style={{ minHeight: "70vh" }}>
-      <Toaster />
-      
-      {children}
-    </main>
-    <Footer />
-  </div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh", marginTop: "65px" }}>
+        <Toaster />
+
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
 
